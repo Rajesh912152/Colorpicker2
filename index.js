eee56e9fc78a2bdb9017458ee2fd275e5ae6885f
Rajesh912152 <121996259+Rajesh912152@users.noreply.github.com>
2023-12-21 15:52:02 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+let containerEl=document.getElementById("container");
+let spanEl=document.getElementById("name");
+
+function button1(){
+    containerEl.style.backgroundColor="#e0e0e0"
+    spanEl.textContent="#e0e0e0"
+}
+function button2(){
+    containerEl.style.backgroundColor="#6fcf97"
+    spanEl.textContent="#6fcf97"
+}
+    
+
+function button3(){
+    containerEl.style.backgroundColor="#56ccf2"
+    spanEl.textContent="#56ccf2"
+
+}
+function button4(){
+    containerEl.style.backgroundColor="#bb6bd9"
+    spanEl.textContent="#bb6bd9"
+
+}
